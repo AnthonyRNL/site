3aeaf14036117c6a220a33598603016b47fd113b
github username <github email address>
2016-04-19 15:48:22 -0400

    route changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express")
 var app = express()
 var ejs = require("ejs")
 var port = process.env.PORT || 3000;
-app.set("view-engine", "ejs")
+// app.set("view-engine", "ejs")
 app.use(express.static("public"))
 
 app.get('/robots.txt', function (req, res) {
@@ -11,11 +11,13 @@ app.get('/robots.txt', function (req, res) {
 });
 
 app.get("/", function(req,res){
-  res.render("index.ejs")
+  // res.render("index.ejs")
+  res.sendFile("/public/index.html")
 })
 
 app.get("/hi", function(req,res){
-  res.render("missingno1.ejs")
+  // res.render("missingno1.ejs")
+  res.sendFile(__dirname + "/public/missingno1.html")
 })
 
 app.listen(port, function(){

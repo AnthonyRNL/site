4f17b763bf3dc85040ab30e5f96d92ab7692f9a2
github username <github email address>
2016-04-19 17:07:04 -0400

    do test

--- server.js
+++ server.js
@@ -5,15 +5,25 @@ var port = process.env.PORT || 3000;
 // app.set("view-engine", "ejs")
 app.use(express.static("public"))
 
-app.get("/", function(req,res){
+
+app.get("/gah", function(req,res){
   // res.render("index.ejs")
-  res.sendFile("/public/index.html")
+  res.sendFile(__dirname + "/public/bah.html")
+})
+
+app.get("/hum", function(req,res){
+  res.sendFile(__dirname + '/public/missingno1.html')
 })
 
 app.get("/hi", function(req,res){
   // res.render("missingno1.ejs")
   res.sendFile(__dirname + "/public/missingno1.html")
 })
+
+app.get('/robots.txt', function (req, res) {
+  res.type('text/plain');
+  res.send("User-agent: *\nDisallow: /");
+});
 
 app.listen(port, function(){
   console.log("heyy, i'm listening...")
